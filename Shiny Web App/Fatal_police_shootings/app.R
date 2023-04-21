72fbdd5ff2bd6e7a9b60cdfba918c7fa2a16b93a
Ivanbhub <bayingana12@gmail.com>
2023-04-20 19:39:24 -0500

    Update app.R

--- Shiny Web App/Fatal_police_shootings/app.R
+++ Shiny Web App/Fatal_police_shootings/app.R
@@ -1,37 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-
-# TO DO :
-
-
-
-# treat level : Add Description 
-
-# 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 library(shiny)
